f60666463acb252406036f6a1aef23dfee9018c7
probonopd <probonopd@users.noreply.github.com>
2020-11-08 09:20:53 +0100

    Update init.sh

--- overlays/ramdisk/init.sh
+++ overlays/ramdisk/init.sh
@@ -17,7 +17,8 @@ mount -u -w /
 
 echo "==> Make mountpoints"
 mkdir -p /cdrom
-mkdir -p /live
+mkdir -p /sysroot
+mkdir -p /memdisk
 
 echo "Waiting for FURYBSD media to initialize"
 while : ; do
@@ -27,23 +28,25 @@ done
 
 echo "==> Mount /cdrom"
 mount_cd9660 /dev/iso9660/FURYBSD /cdrom
-echo "==> Mount /live"
-mdmfs -P -F /cdrom/data/system.uzip -o ro md.uzip /live
+
+echo "==> Mount /sysroot"
+mdmfs -P -F /cdrom/data/system.uzip -o ro md.uzip /sysroot # FIXME: This does not seem to work; why?
 
 if [ "$SINGLE_USER" = "true" ]; then
 	echo -n "Enter memdisk size used for read-write access in the live system: "
 	read MEMDISK_SIZE
 else
-	MEMDISK_SIZE="1024"
+	MEMDISK_SIZE="2048"
 fi
 
-echo "==> Mount swap-based memdisk"
+echo "==> Mount unionfs"
 mdmfs -s "${MEMDISK_SIZE}m" md /memdisk || exit 1
-mount -t unionfs /memdisk /live
+kldload unionfs
+mount -t unionfs /memdisk /sysroot
 
-echo "==> Change into /live"
-mount -t devfs devfs /live/dev
-chroot /livecd /usr/local/bin/furybsd-init-helper
+echo "==> Change into /sysroot"
+mount -t devfs devfs /sysroot/dev
+chroot /sysroot /usr/local/bin/furybsd-init-helper
 
 if [ "$SINGLE_USER" = "true" ]; then
 	echo "Starting interactive shell after chroot ..."
