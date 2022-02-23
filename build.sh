7f51fa5382eade785456e39bddc1259534906d3d
probonopd <probonopd@users.noreply.github.com>
2022-02-23 20:34:12 +0100

    Move portsnap to Developer Tools
    Developer Tools are needed to use Ports

--- build.sh
+++ build.sh
@@ -416,7 +416,8 @@ developer()
   sed -i '' -e 's|^\./usr/local/share/gir-.*|& # developer44|' "${livecd}"/spec.annotated
   sed -i '' -e 's|^\./Applications/Utilities/BuildNotify.app.*|& # developer45|' "${livecd}"/spec.annotated
   sed -i '' -e 's|^\./Applications/Autostart/BuildNotify.app.*|& # developer46|' "${livecd}"/spec.annotated
-  
+  sed -i '' -e 's|^\./usr/sbin/portsnap\ .*|& # developer47|' "${livecd}"/spec.annotated
+   
   cp "${livecd}"/spec.annotated "${livecd}"/spec.user
   cp "${livecd}"/spec.annotated "${livecd}"/spec.developer
 
