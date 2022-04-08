58ad362d1ef8b1b619b630a72b98271da0658f89
probonopd <probonopd@users.noreply.github.com>
2022-04-08 21:42:31 +0200

    13.1-RC2

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ echo "Host system version: ${version}"
 if [ "${version}" = "13.0-RELEASE" ] ; then
   # version="13.0-RC3"
   # version="13.0-RELEASE"
-  version="13.1-RC1"
+  version="13.1-RC2"
 fi
 
 VER=$(echo "${version}" | cut -d "-" -f 1) # "12.2" or "13.0"
