89bebd914650fc80abf88819336a0920463f8823
probonopd <probonopd@users.noreply.github.com>
2022-07-07 22:44:17 +0200

    Use release_1 for Nvidia as well; and here we need txz not pkg...!

--- build.sh
+++ build.sh
@@ -305,7 +305,7 @@ initgfx()
   /usr/local/sbin/pkg-static -c ${uzip} update # Needed if we are shipping additional repos 
   if [ "${arch}" != "i386" ] ; then
     if [ $MAJOR -lt 14 ] ; then
-      PKGS="quarterly"
+      PKGS="release_1"
       # PKGS="latest" # This must match what we specify in packages()
     else
       PKGS="latest"
@@ -314,7 +314,8 @@ initgfx()
     # 390 needed for Nvidia Quadro 2000, https://github.com/helloSystem/hello/discussions/241#discussioncomment-1599131
     # 340 needed for Nvidia 320M
     for ver in '' 390 340 304; do
-        pkgfile=$(/usr/local/sbin/pkg-static -c ${uzip} rquery %n-%v.pkg nvidia-driver${ver:+-$ver})
+        pkgfile=$(/usr/local/sbin/pkg-static -c ${uzip} rquery %n-%v.txz nvidia-driver${ver:+-$ver})
+        # pkgfile=$(/usr/local/sbin/pkg-static -c ${uzip} rquery %n-%v.pkg nvidia-driver${ver:+-$ver})
         fetch -o "${cache}/" "https://pkg.freebsd.org/FreeBSD:${MAJOR}:amd64/${PKGS}/All/${pkgfile}"
         mkdir -p "${uzip}/usr/local/nvidia/${ver:-latest}/"
         tar xfC "${cache}"/${pkgfile} "${uzip}/usr/local/nvidia/${ver:-latest}/"
